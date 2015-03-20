47c6defa09e076dcb545878e854fb98a1b945e1a
Andrei Nadejde <andrei.nadejde@gmail.com>
2015-03-20 20:36:55 +0000

    Hold the initial tableau in class

--- lib/simplex.rb
+++ lib/simplex.rb
@@ -1,2 +1,8 @@
+require 'matrix'
+
 class Simplex
+    attr_accessor :initial_tableau
+    def initialize( initial_tableau )
+        @initial_tableau = Matrix.rows( initial_tableau )
+    end
 end
--- spec/simplex_spec.rb
+++ spec/simplex_spec.rb
@@ -5,4 +5,10 @@ describe Simplex do
     class_type = Simplex
     Simplex.class.should == Class
   end
+  
+  it 'should hold initial tableau in Matrix object' do
+    matrix = [ [ 2, 2 ], [ 1, 2 ] ] 
+    simplex = Simplex.new( matrix )
+    simplex.initial_tableau.should == Matrix.rows( matrix )
+  end
 end
