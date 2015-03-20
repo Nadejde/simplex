e164f3947d052754bd9249d59c6063f2ab985854
Andrei Nadejde <andrei.nadejde@gmail.com>
2015-03-20 22:22:52 +0000

    Calculates basic solution and checks for negative surplus variables

--- lib/simplex.rb
+++ lib/simplex.rb
@@ -1,8 +1,32 @@
 require 'matrix'
 
 class Simplex
-    attr_accessor :initial_tableau
-    def initialize( initial_tableau )
-        @initial_tableau = Matrix.rows( initial_tableau )
+  attr_accessor :tableau
+    
+  def initialize( initial_tableau )
+    @tableau = Matrix.rows( initial_tableau )
+  end
+    
+  def basic_solution 
+    @basic_solution = []
+    
+    @tableau.column_vectors[0..-2].each do |column| #last columns is Ans. not interested in that
+      if column.count { |value| value != 0 } > 1 
+        @basic_solution << 0 
+      else
+        variable_row_index = column.find_index { |value| value != 0 } 
+        @basic_solution << @tableau[variable_row_index, -1] / column[variable_row_index] 
+      end 
     end
+    
+    @basic_solution 
+  end
+  
+  def negative_surplus_variables?
+    true if @tableau.minor( 0..-2, ( variable_count...-1)).find_index { |value| value < 0 }
+  end
+  
+  def variable_count
+    @tableau.column_count - @tableau.row_count - 1
+  end  
 end
--- spec/simplex_spec.rb
+++ spec/simplex_spec.rb
@@ -1,14 +1,49 @@
 require 'simplex'
 
 describe Simplex do
+  before :each do
+    #most test will use the initial tableau for this problem
+    # p = 2x + 3y + z
+    #  x + y + z <= 40 
+    # 2x + y - z => 10 
+    # - y + z => 10 
+    # x => 0, y => 0, z => 0 
+    @initial_tableau = 
+    [
+      [1, 1, 1, 1, 0, 0, 0, 40],
+      [2, 1, -1, 0, -1, 0, 0, 10],
+      [0, -1, 1, 0, 0, -1, 0, 10],
+      [-2, -3, -1, 0, 0, 0, 1, 0]
+      ]
+  end
+  
   it 'should exist' do
     class_type = Simplex
     Simplex.class.should == Class
   end
   
   it 'should hold initial tableau in Matrix object' do
-    matrix = [ [ 2, 2 ], [ 1, 2 ] ] 
-    simplex = Simplex.new( matrix )
-    simplex.initial_tableau.should == Matrix.rows( matrix )
+    simplex = Simplex.new( @initial_tableau )
+    simplex.tableau.should == Matrix.rows( @initial_tableau )
+  end
+  
+  it 'should calculate basic solution for tableau' do
+    # A basic solution is calculated by looking for columns 
+    # with only one value != 0. 
+    # Get row index for that one value.
+    # Variable attached to that colum is Ans[rowindex] / column[rowindex]
+    simplex = Simplex.new( @initial_tableau )
+    simplex.basic_solution.should == [0, 0 ,0, 40, -10, -10, 0]
+  end
+  
+  it 'should detect negative surplus values' do
+    #negative surplus values are not permited for a feasible solution
+    simplex = Simplex.new( @initial_tableau )
+    simplex.negative_surplus_variables?.should == true
+  end
+  
+  it 'should count number of variables' do
+    simplex = Simplex.new( @initial_tableau)
+    simplex.variable_count.should == 3
   end
 end
