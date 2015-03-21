33019250d7ea0c8cff399cc468eada725752d303
Andrei Nadejde <andrei.nadejde@gmail.com>
2015-03-21 09:34:41 +0000

    Figures out if solution is feasible and finds pivog column while slack variables are negative

--- lib/simplex.rb
+++ lib/simplex.rb
@@ -10,28 +10,42 @@ class Simplex
   def basic_solution 
     @basic_solution = []
     
-    @tableau.column_vectors[0..-2].each do |column| #last columns is Ans. not interested in that
+    #last columns is Ans. not interested in that
+    @tableau.column_vectors[0..-2].each do |column| 
       if column.count { |value| value != 0 } > 1 
         @basic_solution << 0 
       else
         variable_row_index = column.find_index { |value| value != 0 } 
-        @basic_solution << @tableau[variable_row_index, -1] / column[variable_row_index] 
+        @basic_solution << 
+            @tableau[variable_row_index, -1] / column[variable_row_index] 
       end 
     end
     
     @basic_solution 
   end
   
-  def negative_surplus_variables?
-    #returns first row found(if any)
-    found_index =
-            @tableau.minor( 0..-2, ( variable_count...-1))
-            .find_index { |value| value < 0 }
-    
-    found_index.first unless found_index.nil?
+  def star_row?
+    #looks for negative surpluss values and returns first row
+    return nil unless column = @basic_solution.find_index { |value| value < 0 }
+    found_index = @tableau.column( column ).find_index { |value| value != 0}
   end
   
   def variable_count
     @tableau.column_count - @tableau.row_count - 1
-  end  
+  end 
+  
+  def feasible_solution?
+    return false if @basic_solution.find_index { |value| value < 0 }
+    return true
+  end
+  
+  def pivot_column
+    if star_row = star_row?
+      #ignore last column (ans)
+      column = @tableau.row( star_row ).
+            find_index( @tableau.row( star_row )[0..-2].max )
+    end
+    
+    return column
+  end
 end
--- spec/simplex_spec.rb
+++ spec/simplex_spec.rb
@@ -19,11 +19,13 @@ describe Simplex do
   
   it 'should exist' do
     class_type = Simplex
+    
     Simplex.class.should == Class
   end
   
   it 'should hold initial tableau in Matrix object' do
     simplex = Simplex.new( @initial_tableau )
+    
     simplex.tableau.should == Matrix.rows( @initial_tableau )
   end
   
@@ -33,13 +35,16 @@ describe Simplex do
     # Get row index for that one value.
     # Variable attached to that colum is Ans[rowindex] / column[rowindex]
     simplex = Simplex.new( @initial_tableau )
+    
     simplex.basic_solution.should == [0, 0 ,0, 40, -10, -10, 0]
   end
   
   it 'should detect negative surplus variables and return firs found row index' do
     #negative surplus values are not permited for a feasible solution
     simplex = Simplex.new( @initial_tableau )
-    simplex.negative_surplus_variables?.should == 1
+    simplex.basic_solution
+    
+    simplex.star_row?.should == 1
   end
   
   it 'should not return row index if no negateive surplus variables' do
@@ -50,13 +55,36 @@ describe Simplex do
       [0, -1, 1, 0, 0, 1, 0, 10],
       [-2, -3, -1, 0, 0, 0, 1, 0]
       ]
-      
     simplex = Simplex.new( tableau )
-    simplex.negative_surplus_variables?.should be_nil
+    simplex.basic_solution
+    
+    simplex.star_row?.should be_nil
   end
   
   it 'should count number of variables' do
     simplex = Simplex.new( @initial_tableau)
+    
     simplex.variable_count.should == 3
   end
+  
+  it 'should know solution is not feasible while slack variables are negative' do
+    simplex = Simplex.new( @initial_tableau )
+    simplex.basic_solution #calculate basic solution
+    
+    simplex.feasible_solution?.should == false
+  end
+  
+  it 'should find correct pivot column while slack variables are negative' do
+    simplex = Simplex.new( @initial_tableau )
+    simplex.basic_solution
+    
+    simplex.pivot_column.should == 0
+  end
+  
+  #it 'should find correct pivot row in column' do
+  #  simplex = Simple.new( @initial_tableau )
+  #  simplex.basic_solution
+    
+  #  simplex.pivot_row( simplex.pivot_column ).should == 1
+  #end
 end
